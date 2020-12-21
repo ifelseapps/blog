5aa9e3ea6324a3421f930ec1e0a900f905953255
Maxim Pavlov <ifelseapps@gmail.com>
2020-12-21 20:20:58 +0300

    Разметка главной страницы

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/css');
+
   return {
     dir: {
+      input: 'src',
       includes: '_includes',
-      layouts: '_layouts'
+      layouts: '_layouts',
     }
   };
 };
