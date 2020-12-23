4287a51f32ee8b8cede28e11cdb210c229884560
Maxim Pavlov <ifelseapps@gmail.com>
2020-12-23 18:52:09 +0300

    Раскладка блоков

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,18 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/css');
 
+  eleventyConfig.addLiquidTag('note', function (liquidEngine) {
+    return {
+      parse: function (tagToken, remainingTokens) {
+        this.str = tagToken.args;
+      },
+      render: function (scope, hash) {
+        const str = liquidEngine.evalValue(this.str, scope);
+        return Promise.resolve(`<p class="note">${str}</p>`);
+      }
+    };
+  });
+
   return {
     dir: {
       input: 'src',
